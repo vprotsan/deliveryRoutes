2c7c344508f0782fdb61200be767e12ce4a28411
Valeria Protsan <valeriyapro@gmail.com>
2020-05-25 09:40:20 -0400

    fully connected to rails

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,4 @@
 class Track < ApplicationRecord
+  belongs_to :user
+  belongs_to :driver
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create( firstName: "Snow", lastName: "Flake", Addr1: "13300 Atlantic blvd" , Addr2: "" , City: "Jacksonville" , State:"FL" , Zip:"31324" )
+User.create( firstName: "Spring", lastName: "Chicken", Addr1: "3637 Shawnee shores dr" , Addr2: "" , City: "Jacksonville" , State:"FL" , Zip:"31324" )
+
+Driver.create( firstName: "Super", lastName: "Driver", Addr1: "3637 Shawnee shores dr" , Addr2: "" , City: "Jacksonville" , State:"FL" , Zip:"31324" )
+
+Store.create( Name: "Pizza", Addr1: "3637 Shawnee shores dr" , Addr2: "" , City: "Jacksonville" , State:"FL" , Zip:"31324" )
