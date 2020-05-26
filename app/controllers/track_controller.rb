4809f10e0be44d9b60456cdac41aaf32984f4be6
Valeria Protsan <valeriyapro@gmail.com>
2020-05-25 21:31:48 -0400

    controller

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -1,40 +1,25 @@
 class TrackController < ApplicationController
+
   # def index
-  #   @codes = Code.all.paginate(page: params[:page], per_page: 10)
-  # end
-  #
-  # def show
-  #   code = params[:format].present? ? params[:code_id]+'.'+params[:format] : params[:code_id]
-  #   @code = Code.find_by(code_id: code )
-  #   @children = @code.children
-  #
-  #
-  #   render json: { code: @code, children: @children }
-  # end
-  # def create
-  #   @newRoute = Track.new(user_id: fav_params[:user_id], code: @code)
-  #
-  #   if @newRoute.save
-  #     render json: @newRoute
-  #   else
-  #     render json: @newRoute.errors, status: :unprocessable_entity
-  #   end
-  #
+  #   track = Track.all
+  #   render json: track, only: [:id, :name], include: :pokemons
   # end
 
   # def show
-  #   @favorites = Favorite.where(user_id: params[:user_id])
-  #   p codes_array = []
-  #   @favorites.each do |favorite|
-  #     code = Code.find(favorite[:code_id])
-  #     p codes_array << { "code_id": code[:code_id], "desc": code[:desc]}
-  #   end
-  #   render json: codes_array
+  #   track = params[:format].present? ? params[:track_id]+'.'+params[:format] : params[:track_id]
+  #   @track = Track.find_by(track_id: track )
+  #
+  #   render json: { track: @track }
   # end
+
+  def index
+    @track = Track.where(user_id: params[:user_id])
+    p tracks_array = []
+    @track.each do |track|
+      track = Track.find(track[:user_id])
+      p tracks_array << { "track_id": track}
+    end
+    # render json: tracks_array
+  end
 
 end
-
-private
-  # def codes_params
-  #   params.require(:code).permit :desc, :code_id
-  # end
